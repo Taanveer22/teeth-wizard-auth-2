3610a5be0792fa5ae30ae2b591fbf21e423ac9bc
Taanveer22 <taanveer469@gmail.com>
2025-12-23 23:09:19 +0600

    alhamdulliah banner section added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import bannerPhoto from "../assets/banner.png";
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero bg-base-300">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={bannerPhoto} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Mir Dental</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
