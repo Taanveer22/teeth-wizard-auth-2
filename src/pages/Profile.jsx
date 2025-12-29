7bcc494224bbbe564e05a18e68ec92745f26eaf3
Taanveer22 <taanveer469@gmail.com>
2025-12-29 23:01:10 +0600

    alhamdulliah profile updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useContext(AuthContext);
+  return (
+    <div>
+      <div className="card bg-base-300 shadow-sm">
+        <h1 className="text-2xl font-medium text-center mt-3">User Profile Data</h1>
+        <figure className="px-10 pt-10">
+          <img src={user?.photoURL} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{user?.displayName}</h2>
+          <p>{user?.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
